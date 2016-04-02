c0b7a6e0a960be3457190b5e93abd8ac3e264979
dittonjs <jditton.atomic@gmail.com>
2016-04-02 08:00:07 -0600

    setup project and gitignore

---
+++ GeoJotsProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GeoJotsProject
+//
+//  Created by Joseph Ditton on 4/2/16.
+//  Copyright © 2016 Joseph Ditton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
